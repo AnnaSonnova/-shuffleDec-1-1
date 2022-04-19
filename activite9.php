20dbd2ad90cfdc695099892f1f1d05d5187daa2b
AnnaSonnova <102247563+AnnaSonnova@users.noreply.github.com>
2022-04-19 16:59:35 -0400

    Update activite9.php
    ajout info fait en class

---
+++ activite9.php
@@ -0,0 +1,12 @@
+<?php
+$num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+$suite = [ "♦", "♣", "♥", "♠"];
+$deck = [];
+for ($i=0; $i < count($suite); $i++) {
+    for ( $j = 0; $j < count($num); $j++) {
+        $deck[] = $num[$j]." ".$suite[$i];
+        
+    }
+}
+print_r($deck[$x]) ;
+        echo "<br>";
